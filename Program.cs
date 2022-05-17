e02d4f8bcaf236df1544d92f28d40071934d9a93
Brad Robinson <contact@toptensoftware.com>
2022-05-17 12:12:41 +1000

    Initial

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using SkiaSharp;
+using System.IO;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bmp = Load(Path.Combine(ThisDir(), "testimage.png"));
+            Save(bmp, Path.Combine(ThisDir(), "testimage_resave.png"));
+        }
+
+        static SKBitmap Load(string filename)
+        {
+            using (var file = File.OpenRead(filename))
+            {
+                using (var skStream = new SKManagedStream(file))
+                {
+                    return SKBitmap.Decode(skStream);
+                }
+            }
+        }
+
+        static void Save(SKBitmap bmp, string filename)
+        {
+            // create an image and then get the PNG (or any other) encoded data
+            using (var image = SKImage.FromBitmap(bmp))
+            {
+                using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+                {
+                    // save the data to a stream
+                    using (var stream = File.OpenWrite(filename))
+                    {
+                        data.SaveTo(stream);
+                    }
+                }
+            }
+        }
+
+        private static string ThisDir([System.Runtime.CompilerServices.CallerFilePath] string path = null)
+        {
+            return System.IO.Path.GetDirectoryName(path);
+        }
+    }
+}
